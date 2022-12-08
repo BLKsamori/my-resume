81ef61d0374493abe14f47feed2e2c19b1f04661
avivd <aviv.desta.dev@gmail.com>
2022-12-08 15:39:27 +0200

    Updates

---
+++ precache-manifest.d4bb337c4629d341695aa77a95087c36.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "b84a2f292a0856a10282",
+    "url": "/resumeBs/static/css/main.9e1b4807.chunk.css"
+  },
+  {
+    "revision": "b84a2f292a0856a10282",
+    "url": "/resumeBs/static/js/main.b84a2f29.chunk.js"
+  },
+  {
+    "revision": "c04e2171de7893cf95d6",
+    "url": "/resumeBs/static/js/1.c04e2171.chunk.js"
+  },
+  {
+    "revision": "5f9cc76307fa4e6b9c1f",
+    "url": "/resumeBs/static/js/2.5f9cc763.chunk.js"
+  },
+  {
+    "revision": "b0804576725d92ed583c",
+    "url": "/resumeBs/static/js/runtime~main.b0804576.js"
+  },
+  {
+    "revision": "47b534e788122c916f299d0a8e077ec4",
+    "url": "/resumeBs/static/media/my_Portrait_1.47b534e7.png"
+  },
+  {
+    "revision": "7850e840edb9b4ddcf775c58111836fc",
+    "url": "/resumeBs/static/media/maintenance.7850e840.png"
+  },
+  {
+    "revision": "38b9efe01c2e9e2ff8fe4e7943621974",
+    "url": "/resumeBs/static/media/CoinCentral_1.38b9efe0.png"
+  },
+  {
+    "revision": "5af26ad4b42b7a2a45a7d451d4035417",
+    "url": "/resumeBs/static/media/MyPortfolio_1.5af26ad4.png"
+  },
+  {
+    "revision": "80220ab4bd185e20d3795d0ec9aa7fd7",
+    "url": "/resumeBs/static/media/Feeds_Now.80220ab4.png"
+  },
+  {
+    "revision": "9ea2d5e4f4a1a7a2972bdb7c282efb62",
+    "url": "/resumeBs/static/media/PHP.9ea2d5e4.png"
+  },
+  {
+    "revision": "ee17fd76442252c9776991cb53f6f7e3",
+    "url": "/resumeBs/static/media/Angular.ee17fd76.png"
+  },
+  {
+    "revision": "5185ffb5c381dad35a342448e6108208",
+    "url": "/resumeBs/static/media/TypeScript.5185ffb5.png"
+  },
+  {
+    "revision": "636616effb25380ce5e13c8110fbec74",
+    "url": "/resumeBs/static/media/jQuery.636616ef.png"
+  },
+  {
+    "revision": "637a8c19d4070415ce4578cbaf4bf339",
+    "url": "/resumeBs/static/media/Nodejs.637a8c19.png"
+  },
+  {
+    "revision": "1e4cc8ad56d815640646dddfcc86a538",
+    "url": "/resumeBs/static/media/React.1e4cc8ad.png"
+  },
+  {
+    "revision": "8a41d8ff164fadc8de8b5e4f61034bff",
+    "url": "/resumeBs/static/media/Email.8a41d8ff.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/resumeBs/static/media/LinkedIn.fd0d5546.png"
+  },
+  {
+    "revision": "b51edbace037db8c5f734237193e1c36",
+    "url": "/resumeBs/static/media/GitHub.b51edbac.png"
+  },
+  {
+    "revision": "9c193be60c3162ca892c417fec64862d",
+    "url": "/resumeBs/static/media/Moon.9c193be6.png"
+  },
+  {
+    "revision": "7c4729a6c7c0a6d02015ed56a6d6d9df",
+    "url": "/resumeBs/static/media/Sun.7c4729a6.png"
+  },
+  {
+    "revision": "701313d2760a42c2abc7a4181777bac0",
+    "url": "/resumeBs/index.html"
+  }
+];
